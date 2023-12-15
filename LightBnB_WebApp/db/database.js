5c0fe1b90bd92142ecde3cf7f187e1d817b3728f
YoasterToaster <joefoschia@gmail.com>
2023-12-15 00:14:04 -0500

    Add test_code file and refactored getAllProperties to work with the form

--- LightBnB_WebApp/db/database.js
+++ LightBnB_WebApp/db/database.js
@@ -101,18 +101,51 @@ const getAllReservations = (guest_id, limit = 10) => {
  * @param {*} limit The number of results to return.
  * @return {Promise<[{}]>}  A promise to the properties.
  */
-// const getAllProperties = (options, limit = 10) => {
 
 const getAllProperties = (options, limit = 10) => {
-  return pool
-    .query(`SELECT * FROM properties LIMIT $1`, [limit])
-    .then((result) => {
-      console.log(result.rows);
-      return result.rows;
-    })
-    .catch((err) => {
-      console.log(err.message);
-    });
+   // 1
+   const queryParams = [];
+   // 2
+   let queryString = `
+   SELECT properties.*, avg(property_reviews.rating) as average_rating
+   FROM properties
+   JOIN property_reviews ON properties.id = property_id
+   `;
+ 
+   // 3
+   if (options.city) {
+     queryParams.push(`%${options.city}%`);
+     queryString += `WHERE city LIKE $${queryParams.length} `;
+   }
+   if (options.owner_id){
+    queryParams.push(options.owner_id);
+    queryString += `AND owner_id = $${queryParams.length} `
+   }
+   if (options.minimum_price_per_night && options.maximum_price_per_night){
+    queryParams.push(options.minimum_price_per_night);
+    queryString += `AND cost_per_night >= ($${queryParams.length} * 100) `
+    queryParams.push(options.maximum_price_per_night);
+    queryString += `AND cost_per_night <= ($${queryParams.length} * 100) `
+  }
+  let minRating= '';
+  if (options.minimum_rating){
+    queryParams.push(options.minimum_rating);
+    minRating = `HAVING avg(property_reviews.rating) >= $${queryParams.length} `
+  }
+   // 4
+   queryParams.push(limit);
+   queryString += `
+   GROUP BY properties.id
+   ${minRating}
+   ORDER BY cost_per_night
+   LIMIT $${queryParams.length};
+   `;
+ 
+   // 5
+   console.log(queryString, queryParams);
+ 
+   // 6
+   return pool.query(queryString, queryParams).then((res) => res.rows);
 };
 
 // the following assumes that you named your connection variable `pool`
@@ -128,7 +161,10 @@ const addProperty = function (property) {
   properties[propertyId] = property;
   return Promise.resolve(property);
 };
-// getAllReservations(50, 10).then(response => {console.log(response)});
+getAllProperties({city: 'Vancouver', owner_id: 222,  minimum_price_per_night: 5,
+maximum_price_per_night: 500000,
+minimum_rating: 4}, 1).then(response => {console.log(response)});
+
 module.exports = {
   getUserWithEmail,
   getUserWithId,
